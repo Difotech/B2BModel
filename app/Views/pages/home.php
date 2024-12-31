2c8aaf41c988f8af9add6881906a6234d7be7514
difotech <vitodifonzo@difotech.it>
2024-12-31 15:08:24 +0100

    home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,13 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prodotti pugliesi nel mondo</title>
-    <link rel="stylesheet" href="<?= base_url('public/style.css'); ?>" type="text/css">
 
-</head>
 <body>
-    <h1>Prodotti pugliesi nel mondo</h1>
+<div id="sez_principale">
+
+         <div id="sez_home">
+                 <div class="containerhome">
+                    
+                          <div class="texthome">
+                                <h2><b>MURGIAPIZZA</b> - Vendita di <strong>basi pizza per locali commerciali</strong>
+                                </h2>
+                                <h2><strong>BASI PER PIZZA</strong>, PUCCE E FOCACCE PRODOTTE IN <strong>ITALIA</strong></h2>
+                                
+                                 <a href="https://www.murgiapizza.com/sezioni/catalogo.html"><center> <button class="bottone"><span>VAI AL CATALOGO</span></button></center></a>
+                          </div>
+                          
+                            <div class="imagehome">
+                         
+                            <video class="video-overlay" autoplay muted loop  controls>
+                                    <source src="./video/home.mp4" type="video/mp4">
+                                        Your browser does not support the video tag.
+                                  </video>
+                             </div>
+                      
+                </div>
+         
+        </div>
+        
+        <center>
+        <p id="testoSpeciale">
+             I nostri prodotti di alta qualità con ingredienti della nostra amata <strong>Puglia</strong>.
+             <br>
+            Sulla nostra piattaforma puoi richiedere un preventivo per:
+           <br> <strong>base per pizza</strong>, <strong>base per pizza rettangolare</strong>, <strong>puccia</strong>, <strong>focaccia barese pronta</strong>, <strong>panzerotto</strong>, <strong>pinsa romana</strong>, <b>saltimbocca</b> e <b>ciabatta</b>.
+             </p></center>
+             <!-- Aggiunta dello slideshow con 4 immagini iniziali affiancate -->
+
+    <div class="slideshow-container">
+      <div class="mySlides">
+      <a href="./sezioni/catalogo.html" target="_blank">   <img class="slideshow-image" src="./immagini/catalogo/22a.webp" alt="Slide 1" width="500" height="350"></a>
+      </div>
+      <div class="mySlides">
+         <a href="./sezioni/catalogo.html" target="_blank">  <img class="slideshow-image" src="./immagini/catalogo/9b.webp" alt="Slide 2" width="500" height="350"></a>
+      </div>
+      <div class="mySlides">
+        <a href="./sezioni/catalogo.html" target="_blank">   <img class="slideshow-image" src="./immagini/catalogo/6b.webp" alt="Slide 3" width="500" height="350"></a>
+      </div>
+      <div class="mySlides">
+          <a href="./sezioni/catalogo.html" target="_blank"> <img class="slideshow-image" src="./immagini/catalogo/13a.webp" alt="Slide 4" width="500" height="350"></a>
+      </div>
+<div class="mySlides">
+           <a href="./sezioni/catalogo.html" target="_blank"><img class="slideshow-image" src="./immagini/catalogo/6a.webp" alt="Slide 5"></a>
+      </div>
+      <div class="mySlides">
+           <a href="./sezioni/catalogo.html" target="_blank"><img class="slideshow-image" src="./immagini/catalogo/7a.webp" alt="Slide 5"></a>
+      </div>
+      <div class="mySlides">
+          <a href="./sezioni/catalogo.html" target="_blank"> <img class="slideshow-image" src="./immagini/catalogo/12a.webp" alt="Slide 5"></a>
+      </div>
+      <div class="mySlides">
+         <a href="./sezioni/catalogo.html" target="_blank">  <img class="slideshow-image" src="./immagini/catalogo/3.webp" alt="Slide 5"></a>
+      </div>
+      <div class="mySlides">
+          <a href="./sezioni/catalogo.html" target="_blank"> <img class="slideshow-image" src="./immagini/catalogo/2.webp" alt="Slide 5"></a>
+      </div>
+      <!-- Aggiungi più div per ogni immagine -->
+<br>
+      <!-- Pulsanti di navigazione -->
+      <button class="prev" onclick="plusSlides(-1)">&#10094;</button>
+      <button class="next" onclick="plusSlides(1)">&#10095;</button>
+    </div>
+    <!-- Fine dello slideshow -->
+    
+        <p id="testoSpeciale">
+           <strong>Murgiapizza</strong> è un'azienda che vende <strong>basi per pizza precotte</strong> in tutta <strong>Italia</strong> e all'estero.
+           <br>
+           Per tutti i nostri prodotti sono stati utilizzati ingredienti di qualità del territorio <b>pugliese</b>.
+         </p>
+       <div id="first">
+    <img id="firstfoto" src="./immagini/ita.webp" alt="murgiapizza" width="1220" height="403">      <!-- <button id="bottoneAComparsa">Il Tuo Bottone a Comparsa</button>-->
+   <a href="https://www.murgiapizza.com/sezioni/chi-siamo.html">    <button class="bottonehome"><span>CHI SIAMO</span></button></a>
+        </div>
+  <p id="testoSpeciale">
+           Spedizioni in tutto il mondo in modo facile e veloce.
+            <br>Richiedi subito un preventivo e assapora le  basi preparate <strong>Murgia pizza</strong>.
+         </p>
+   <a href="https://www.murgiapizza.com/sezioni/fai-una-richiesta.html"> <button class="bottone"><span>RICHIEDI UN PREVENTIVO</span></button></a>
+</div>
+
+
+
+  <script src="./js/scriptscorrimento.js"></script>
 </body>
-</html>
+
