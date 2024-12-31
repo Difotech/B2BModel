2cafffaa256a43951f6f4f35efdfb53208fd3340
difotech <vitodifonzo@difotech.it>
2024-12-31 15:06:37 +0100

    footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,14 @@
- <em>&copy; 2023</em>
-        </body>
-</html>
+<footer>
+ <div class="footer">
+     <li id="footer_sez">
+     <a href="./footer/newsletter.html" target="_blanck"> Iscrizione alla newsletter</a> | <a href="./sezioni/contattaci.html" target="_blanck">Contatti</a> | <a href="./footer/privacypolicy.html" target="_blanck">Privacy Policy</a> | <a href="./footer/cookiepolicy.html" target="_blanck">Cookie Policy</a> |  <a href="./footer/disclaimer.html" target="_blanck">Disclaimer</a> | <a href="./footer/terminiecondizioni.html" target="_blanck">Termini e Condizioni</a> | <a href="./footer/pagamenti.html" target="_blanck">Metodi di pagamento</a> | <a href="./footer/spedizioni.html" target="_blanck">Spedizioni</a>
+     </li>
+ </div>
+ 
+ <div class="footer">
+ <div id="footer_sez">
+     © Murgiapizza.com 2023 | contattaci@murgiapizza.com
+      <br>
+        GRUPPO FORMATO DA:
+        Vito Difonzo, Giuseppe Fraccalvieri e Francesco Bitetti
+ </footer>
