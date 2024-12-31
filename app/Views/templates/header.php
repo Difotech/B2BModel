d557c00795548e2395e23f7e86992453cd45aca8
difotech <vitodifonzo@difotech.it>
2024-12-31 15:03:59 +0100

    navbar e css aggiornato

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,6 +1,56 @@
-<html>
+<!--<html>
         <head>
                 <title>CodeIgniter Tutorial</title>
         </head>
         <body>
-       <!--<h1><?= esc($title) ?></h1>-$_COOKIE->
+       --<h1><?= esc($title) ?></h1>-$_COOKIE->
+
+-->
+
+<!DOCTYPE html>
+<html lang="it">
+        <head>
+          <title>Murgiapizza - Vendita di basi pizza all'ingrosso</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="Description" CONTENT="Scopri le autentiche basi per pizza di Santeramo in Colle, Puglia. Semplici da usare e irresistibilmente deliziose. Gusta la Puglia con ogni morso!">
+  
+  <meta name="keywords" content="basi pizza, basi per pizza, puccia, focaccia barese, panzerotto, pinsa romana, saltimbocca, ciabatta, prodotti italiani, prodotti pugliesi, ingredienti di qualità, spedizioni internazionali, basi pizza precotte, basi per puccia, prodotti precotti italiani, base pizza italiana, base pizza napoletana, focaccia barese preparata, murgia pizza, murgiapizza, basi per pizza puglia, basi per pizza Italia, murgia pizza basi pizza, murgia, pizza, prodotti precotti, pizza per ristoranti">
+
+
+  <meta name="robots" content="index, follow" />
+
+<link rel="canonical" href="" />
+
+  <!--CSS, ICON, FONTS-->
+  <link rel="stylesheet" href="<?= base_url('public/style.css'); ?>" type="text/css">
+  <link  rel = "apple-touch-icon"  href="./immagini/favicon.webp" >
+  <link rel="icon" href="<?= base_url('public/img/favicon.webp'); ?>">
+  
+ 
+   
+        </head>
+        
+        
+        <body>
+            
+            
+             <h1 hidden>Murgiapizza - Vendita di basi pizza all'ingrosso</h1>
+    
+  <div class="blog_title">
+    <h2>Le basi pizza e prodotti ideali per la tua attività</h2>
+        <img id="logo" src="<?= base_url('public/img/avatar.webp'); ?>" alt="murgiapizza">
+  </div>
+
+ <nav class="navbar">
+  <ul>
+    <li><a href="">HOME</a></li>
+    <li><a href="./sezioni/chi-siamo.html">CHI SIAMO</a></li>
+    <li><a href="./sezioni/catalogo.html">CATALOGO</a></li>
+    <li><a href="./sezioni/fai-una-richiesta.html">FAI UNA RICHIESTA</a></li>
+    <li><a href="./sezioni/contattaci.html">CONTATTACI</a></li>
+    <li><a href="./sezioni/blog.html">BLOG</a></li>
+  </ul>
+ </nav>
+
