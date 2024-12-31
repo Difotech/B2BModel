a39212d5f1aba719a960a5188fac6ef4b28ab7ad
difotech <vitodifonzo@difotech.it>
2024-12-31 14:51:55 +0100

    Inserito file css di prova

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prodotti pugliesi nel mondo</title>
+    <link rel="stylesheet" href="<?= base_url('public/style.css'); ?>" type="text/css">
+
 </head>
 <body>
     <h1>Prodotti pugliesi nel mondo</h1>
